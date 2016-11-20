9df76d0bb6bf13755bbde87a3373df778ae2ce5b
Rajat <rajatmig29@gmail.com>
2016-11-20 01:29:01 -0800

    Revert "prices in email"
    This reverts commit 8f184451a4e06c0ad09c1a5052fa4ddf0663e75c.

--- sendMail.js
+++ sendMail.js
@@ -1,9 +1,7 @@
 var nodemailer = require("nodemailer");
-var _ = require('lodash');
-var _String = require('underscore.string');
-var fs = require('fs');
+var _ = require('lodash')
 module.exports = {
-    sendEmail: function (Order, UserDetails, PriceDetails, callback) {
+    sendEmail: function (Order,UserDetails,PriceDetails,callback) {
         var transporter = nodemailer.createTransport({
             service: 'Gmail',
             auth: {
@@ -12,81 +10,64 @@ module.exports = {
             }
         });
 
-        var html = "<p style='color:orange'>Greetings " + UserDetails.Name + "</p><br/>"
-            + "Congratulation YOUR order has been placed"
-            + "<br/><div>"
-            + "<b>Order Details: </b>"
-            + "<table style='width: 100%;max-width: 100%;margin-bottom: 20px;background-color: transparent;  border-color: grey;>"
-            + "<tbody>"
-            + "<tr style='background-color:#f9f9f9;'>"
-            + "<th style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>#</th>"
-            + "<th style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>Item Type</th>"
-            + "<th style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>Item</th>"
-            + "<th style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>Price</th>"
-            + "<th style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>Total</th>"
-
-
-        var index = 1
-        fs.readFile('./Prices.json', 'utf8', function (err, data) {
-            if (err) {
-                throw err;
-            } else {
-                obj = JSON.parse(data);
-                _.forEach(Order, function (itemType, key) {
-                    if (key === "extra") {
-                        _.forEach(Order["extra"], function (extraType, extraKey) {
-                            html = html + "<tr><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + index + "</td><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + extraType.SubItemType + "</td><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + extraType.SubItemValue + "</td>"
-                                + "<td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + PriceDetails["extra"][extraKey].Price + "</td>"
-                                + "<td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'></td>"
-                                + "</tr>"
-                            index++;
-                        });
-                    } else {
-                        var image = ""
-                        if (key == "1") {
-                               obj.forEach(function (Item) {
-                                if (_String.include(itemType.SubItemType, Item.Type.toString()) == true) {
-                                    Item.Items.forEach(function (subItem) {
-                                        if (subItem.Type === itemType.SubItemValue) {
-                                            image = subItem.Image
-                                        }
-                                    })
-                                }
-                            }, this);
-                            html = html + "<tr><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + index + "</td><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + itemType.SubItemType + "</td><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + itemType.SubItemValue + "</td>"
-                                + "<td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + PriceDetails.ItemPrice + "</td>"
-                                + "<td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'></td>"
-                                +"<td>"+image+"</td>"
-                                + "</tr>"
+        var html = "<p style='color:orange'>Greetings "+UserDetails.Name+"</p><br/>"
+                    +"Congratulation YOUR order has been placed"
+                    +"<br/><div>"
+                    +"<b>Order Details: </b>"
+                    +"<table style='width: 100%;max-width: 100%;margin-bottom: 20px;background-color: transparent;  border-color: grey;>"
+                    +"<tbody>"
+                    +"<tr style='background-color:#f9f9f9;'>"
+                    +"<th style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>#</th>"
+                    +"<th style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>Item Type</th>"
+                    +"<th style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>Item</th>"
+                    +"<th style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>Price</th>"
+                    +"<th style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>Total</th>"
+                    
+                    
+                    var index = 1
+                    _.forEach(Order,function (itemType,key) {
+                        if (key === "extra") {
+                            _.forEach(Order["extra"],function (extraType,extraKey) {
+                                html = html + "<tr><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + index + "</td><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + extraType.SubItemType + "</td><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" +extraType.SubItemValue + "</td>"
+                                +"<td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>"+PriceDetails["extra"][extraKey].Price+"</td>"
+                                +"<td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'></td>"
+                                +"</tr>"
+                                index++;
+                            });
                         } else {
-                            html = html + "<tr><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + index + "</td><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + itemType.SubItemType + "</td><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + itemType.SubItemValue + "</td>"
-                                + "<td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'></td>"
-                                + "<td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'></td>"
-                                + "</tr>"
+                            if(key=="1"){
+                                html = html +"<tr><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + index + "</td><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + itemType.SubItemType + "</td><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + itemType.SubItemValue + "</td>"
+                                +"<td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + PriceDetails.ItemPrice + "</td>"
+                                +"<td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'></td>"
+                                +"</tr>"
+                            }else{
+                                html = html +"<tr><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + index + "</td><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + itemType.SubItemType + "</td><td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'>" + itemType.SubItemValue + "</td>"
+                                +"<td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'></td>"
+                                +"<td style='padding: 8px;line-height: 1.42857143;vertical-align: top; border-top: 1px solid #ddd;'></td>"
+                                +"</tr>"
+                            }
+                            
                         }
 
-                    }
+                        index++;
+                    });
 
-                    index++;
-                });
-            }
-        });
-        html = html + "<tr><td></td><td></td><td></td><td></td><td>" + PriceDetails.TotalPrice + "</td></tr>";
-        html = html + "</tbody></table></div><br/></br/><div>"
-            + "<b>User Details: </b>"
-            + "<p>Name :" + UserDetails.Name + "</p>"
-            + "<p>Email :" + UserDetails.Email + "</p>"
-            + "<p>Phone :" + UserDetails.Phone + "</p>"
-            + "<p>Address :" + UserDetails.Address + "</p>"
-            + "<p>Pincode :" + UserDetails.Pincode + "</p>"
-            + "<p>Pickup :" + UserDetails.Pickup + "</p>"
-            + "<p>Message :" + UserDetails.Message + "</p>"
-            + "</div><br/><br/><br/>";
-
+                    html= html +"<tr><td></td><td></td><td></td><td></td><td>" + PriceDetails.TotalPrice + "</td></tr>";
+                    html =html +"</tbody></table></div><br/></br/><div>"
+                                +"<b>User Details: </b>"
+                                +"<p>Name :" + UserDetails.Name +"</p>"
+                                +"<p>Email :"+ UserDetails.Email +"</p>"
+                                +"<p>Phone :"+ UserDetails.Phone +"</p>"
+                                +"<p>Address :"+ UserDetails.Address +"</p>"
+                                +"<p>Pincode :"+ UserDetails.Pincode +"</p>"
+                                +"<p>Pickup :"+ UserDetails.Pickup +"</p>"
+                                +"<p>Message :"+ UserDetails.Message +"</p>"
+                                +"</div><br/><br/><br/>";
+        
         var mailOptions = {
             from: 'alterbliss.com@gmail.com', // sender address
             to: UserDetails.Email, // list of receivers separated by comma
-            cc: 'alterbliss.com@gmail.com',
+            cc:'alterbliss.com@gmail.com',
             subject: 'Blis - Order Confirmation', // Subject line
             //text: text //, // plaintext body
             html: html // You can choose to send an HTML body instead
@@ -95,10 +76,10 @@ module.exports = {
         transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
                 console.log(error);
-                callback(error, null)
+                callback(error,null)
             } else {
                 console.log('Message sent: ' + info.response);
-                callback(null, info.response);
+                callback(null,info.response );
             };
         });
     }
